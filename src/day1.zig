6161cf4d19ed65119ab40115b1a05109f5fcdeb0
John te Bokkel <tanj.tanj@gmail.com>
2024-12-16 15:06:16 -0500

    day 1 part 2

--- src/day1.zig
+++ src/day1.zig
@@ -34,7 +34,9 @@ pub fn main() !void {
     const left = try list_left.toOwnedSlice();
     const right = try list_right.toOwnedSlice();
     const distance = find_distance(left, right);
+    const sim = find_sim(left, right);
     try stdout.print("Distance: {d}\n", .{distance});
+    try stdout.print("Similarity: {d}\n", .{sim});
     try bw.flush();
 }
 
@@ -49,10 +51,21 @@ fn find_distance(left: []i64, right: []i64) i64 {
     return sum;
 }
 
+fn find_sim(left: []i64, right: []i64) i64 {
+    var sim: i64 = 0;
+    for (left) |l| {
+        const needle: []const i64 = &.{l};
+        const c: i64 = @intCast(std.mem.count(i64, right, needle));
+        sim += l * c;
+    }
+    return sim;
+}
+
 test "distance" {
     var left = [_]i64{ 3, 4, 2, 1, 3, 3 };
     var right = [_]i64{ 4, 3, 5, 3, 9, 3 };
     var start: usize = 0;
     _ = &start;
-    try std.testing.expectEqual(find_distance(left[start..], right[start..]), 11);
+    try std.testing.expectEqual(11, find_distance(left[start..], right[start..]));
+    try std.testing.expectEqual(31, find_sim(left[0..], right[0..]));
 }
