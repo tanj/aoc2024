c0e90dbe114fb3494f0c8e2718a21fceacd0c973
John te Bokkel <tanj.tanj@gmail.com>
2025-01-26 14:59:12 -0800

    day 9 part 2

--- src/day9.zig
+++ src/day9.zig
@@ -53,6 +53,84 @@ test "day 9 part 1" {
     try std.testing.expectEqual(1928, result);
 }
 
+fn defrag2(allocator: std.mem.Allocator, input: []const u8) !usize {
+    var fs = std.ArrayList(usize).init(allocator);
+    defer fs.deinit();
+    var ix_f: usize = 0;
+    var ix_e: usize = 1;
+    var fh: usize = 0;
+    // generate disk map
+    while (ix_f < input.len) : ({
+        ix_f += 2;
+        ix_e += 2;
+        fh += 1;
+    }) {
+        if (input[ix_f] >= '0' and input[ix_f] <= '9') {
+            const fb: u8 = input[ix_f] - '0';
+            try fs.appendNTimes(fh, fb);
+        }
+        if (ix_e < input.len) {
+            if (input[ix_e] >= '0' and input[ix_e] <= '9') {
+                const eb: u8 = input[ix_e] - '0';
+                try fs.appendNTimes(max_usize, eb);
+            }
+        }
+    }
+    // defrag map
+    var tail: usize = fs.items.len - 1;
+    // fh is currently the next fh so lets make it the current
+    fh -= 1;
+    while (fh > 0) : ({
+        fh -= 1;
+    }) {
+        // find next fh and it's length
+        var len: usize = 0;
+        var fh_start: usize = 0;
+        while (tail > 0 and (fs.items[tail] == max_usize or fs.items[tail] >= fh)) : (tail -= 1) {
+            if (fs.items[tail] == fh) {
+                len += 1;
+                fh_start = tail;
+            }
+        }
+        tail = fh_start;
+        // look for free space
+        var head: usize = 0;
+        var free_start: usize = max_usize;
+        move_file: while (head < fh_start) : (head += 1) {
+            if (fs.items[head] == max_usize) {
+                if (free_start == max_usize) {
+                    free_start = head;
+                }
+                if (head - free_start + 1 == len) {
+                    var ix: usize = 0;
+                    while (ix < len) : (ix += 1) {
+                        fs.items[fh_start + ix] = max_usize;
+                        fs.items[free_start + ix] = fh;
+                    }
+                    break :move_file;
+                }
+            } else {
+                free_start = max_usize;
+            }
+        }
+    }
+    // crc map
+    var checksum: usize = 0;
+    var pos: usize = 0;
+    while (pos < fs.items.len) : (pos += 1) {
+        if (fs.items[pos] != max_usize) {
+            checksum += pos * fs.items[pos];
+        }
+    }
+    return checksum;
+}
+test "day 9 part 2" {
+    //
+    const input: []const u8 = "2333133121414131402";
+    const result = try defrag2(std.testing.allocator, input);
+    try std.testing.expectEqual(2858, result);
+}
+
 pub fn main() !void {
     var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
     defer arena.deinit();
@@ -76,7 +154,7 @@ pub fn main() !void {
 
     const result = try defrag(arena.allocator(), buf[0..read]);
     try stdout.print("Result: {d}\n", .{result});
-    // const result2 = try find_harmonic_antinodes(arena.allocator(), buf[0..read]);
-    // try stdout.print("Result 2: {d}\n", .{result2});
+    const result2 = try defrag2(arena.allocator(), buf[0..read]);
+    try stdout.print("Result 2: {d}\n", .{result2});
     try bw.flush();
 }
